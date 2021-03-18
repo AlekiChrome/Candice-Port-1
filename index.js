826448ca3ba4593ff1de64796e058ac0a9adfb96
Candice Agard <candiceagard@Candices-MBP.home>
2021-03-18 16:34:21 -0400

    Adding all files

---
+++ index.js
@@ -0,0 +1,36 @@
+const splash = document.querySelector('.splash');
+const navToggle = document.querySelector('#navToggle');
+const nav = document.querySelector('nav');
+const navIcon = document.querySelectorAll('.navIcon');
+const hamburger = document.querySelector('#hamburger');
+
+// splash screen
+setTimeout(() => {
+    splash.classList.add('disappear')
+}, 3500);
+
+// hamburger
+navToggle.addEventListener("click", () => {
+    nav.classList.toggle('open');
+    navIcon.forEach(icon => {
+        icon.classList.toggle('hidden');
+    });
+});
+
+window.addEventListener('resize', () => {
+    if (document.body.clientWidth > 720)
+        nav.classList.remove('open');
+        navIcon.forEach(icon => {
+            icon.classList.add('hidden');
+        });
+    hamburger.classList.remove('hidden');
+});
+
+
+// Tilt and glare on cards
+VanillaTilt.init(document.querySelectorAll(".card"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1
+})
