eef3d46d9545c3816ee4f89d64fe228d8cb95c59
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-03-20 09:16:55 -0400

    logo link added, smooth transition added

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const nav = document.querySelector('nav');
 const navIcon = document.querySelectorAll('.navIcon');
 const hamburger = document.querySelector('#hamburger');
 
+const welcomeMessage = () => {
+    console.log(`Hey, So you want to see how things work. \nThat's pretty cool. Since you are lurking, \nmy icon takes you to an organization I work with that supports black artist.
+    \n\tTake a look.`)
+}
+welcomeMessage();
+
 // splash screen
 setTimeout(() => {
     splash.classList.add('disappear')
